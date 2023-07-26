3f811b5f1e5ecd982abcff12ecf8b5ac2c8a3fa5
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-07-26 13:04:15 -0600

    Add functions to book.js.
    Co-authored-by: Gabe Nunez <gabriel.c.nunez@gmail.com>

--- src/book.js
+++ src/book.js
@@ -1,12 +1,27 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return "The " + title;
+}
+
+function buildMainCharacter(fname, fage, fpronouns) {
+  return {
+    name: fname,
+    age: fage,
+    pronouns: fpronouns
+  };
+}
+
+function saveReview(review, array) {
+  if (!array.includes(review)){
+    array.push(review)
+  }
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
 }
+
