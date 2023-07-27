76a7d4f2f8d3cb8716abfb64a809976a5a603dca
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-07-27 10:47:00 -0700

    Complete shelfBook and unshelfBook testing

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,20 @@
+function shelfBook(book, genreShelf) {
+  if (genreShelf.length < 3) {
+    genreShelf.unshift(book);
+  }
+}
+
+function unshelfBook (bookTitle, genreShelf) {
+  const indexToRemove = genreShelf.findIndex(book => book.title === bookTitle);
+  // genreShelf.splice(indexToRemove, 1); -- This only works if bookTitle exists on genreShelf
+  if (indexToRemove !== -1) {
+    genreShelf.splice(indexToRemove, 1);
+  }
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
