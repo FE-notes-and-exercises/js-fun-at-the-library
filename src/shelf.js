cde7052f018f7f75efa8263f915a39c56e8ed77b
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-07-28 13:28:41 -0600

    Add listTitles and searchShelf functions to shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -4,26 +4,52 @@ function shelfBook(book, genreShelf) {
   }
 }
 
-// function unshelfBook (bookTitle, genreShelf) {
-//   const indexToRemove = genreShelf.findIndex(book => book.title === bookTitle);
-//   // genreShelf.splice(indexToRemove, 1); -- This only works if bookTitle exists on genreShelf
-//   if (indexToRemove !== -1) {
-//     genreShelf.splice(indexToRemove, 1);
+function unshelfBook (bookTitle, genreShelf) {
+  const indexToRemove = genreShelf.findIndex(book => book.title === bookTitle);
+  // genreShelf.splice(indexToRemove, 1); -- This only works if bookTitle exists on genreShelf
+  if (indexToRemove !== -1) {
+    genreShelf.splice(indexToRemove, 1);
+  }
+}
+
+// Another way to write function with for loop
+// function unshelfBook(bookTitle, genreShelf) {
+//   for (var i = 0; i < genreShelf.length; i++) {
+//     if (genreShelf[i].title == bookTitle && i !== -1) {
+//       genreShelf.splice(i, 1);
+//     }
 //   }
 // }
 
-// Another way to write function with for loop
-function unshelfBook(bookTitle, genreShelf) {
+function listTitles(genreShelf) {
+  let titles = []
   for (var i = 0; i < genreShelf.length; i++) {
-    if (genreShelf[i].title == bookTitle && i !== -1) {
-      genreShelf.splice(i, 1);
-    }
+    //.push will add element to end of array
+    titles.push(genreShelf[i].title);
+  }
+  return titles.join(', ')
+}
+
+// searchShelf works but should be refactored
+function searchShelf(genreShelf, title) {
+  let count = 0
+
+  for (var i = 0; i < genreShelf.length; i++) {
+    if (genreShelf[i].title == title) {
+      count += 1;
+    } 
+  }
+
+  if (count >= 1) {
+    return true;
+  } else {
+    return false;
   }
 }
 
 module.exports = {
   shelfBook,
   unshelfBook,
-  // listTitles,
-  // searchShelf
+  listTitles,
+  searchShelf
 };
